771a35e6bef6f7e8d93ab21c3faaf431c23e33cc
Cheuk Lam <cheuk.lam@emc.com>
2015-10-29 19:24:54 -0400

    Added support of multiple measurements.

--- core/src/main/java/com/yahoo/ycsb/workloads/TimeSeriesWorkload.java
+++ core/src/main/java/com/yahoo/ycsb/workloads/TimeSeriesWorkload.java
@@ -27,17 +27,19 @@ public class TimeSeriesWorkload extends Workload {
     private TimestampGenerator queryTS;
     private TimeUnit timeUnit;
     private String table;
-    private String measurement;
+    private String measurementPrefix;
+    private int measurementCount;
     private String fieldPrefix;
     private int fieldCount;
-    private static final AtomicInteger currFieldIndex = new AtomicInteger();
+    private static final AtomicInteger index = new AtomicInteger();
     private static final Random rand = new Random();
 
     @Override
     public void init(Properties p) throws WorkloadException {
         timeUnit = TimeUnit.valueOf(p.getProperty("tsdb.timeUnit", "MILLISECONDS"));
         table = p.getProperty("tsdb.table", "mydb");
-        measurement = p.getProperty("tsdb.measurement", "measurement1");
+        measurementPrefix = p.getProperty("tsdb.measurement.prefix", "measurement");
+        measurementCount = Integer.parseInt(p.getProperty("tsdb.measurement.count", "1"));
         fieldPrefix = p.getProperty("tsdb.field.prefix", "field");
         fieldCount = Integer.parseInt(p.getProperty("tsdb.field.count", "1"));
         final String floatGeneratorName = p.getProperty("tsdb.floatGenerator", "fixed");
@@ -54,9 +56,29 @@ public class TimeSeriesWorkload extends Workload {
         queryTS = new RandomTimestampGenerator(floorTS, ceilingTS);
     }
 
+    /**
+     * Compute the corresponding measurement name given an integer id.
+     * @param id
+     * @return Measurement name
+     */
+    private String getMeasurementName(final int id) {
+        return measurementPrefix + (id/fieldCount)%measurementCount;
+    }
+
+    /**
+     * Compute the corresponding field name given an integer id.
+     * @param id
+     * @return Field name
+     */
+    private String getFieldName(final int id) {
+        return fieldPrefix + id%fieldCount;
+    }
+
     @Override
     public boolean doInsert(DB db, Object threadstate) {
-        final String field = fieldPrefix + currFieldIndex.getAndIncrement()%fieldCount;
+        final int id = index.getAndIncrement();
+        final String measurement = getMeasurementName(id);
+        final String field = getFieldName(id);
         final DataPointWithMetricID dp = new DataPointWithMetricID(
                 field, loadTS.next(), new FloatByteIterator(floatGenerator.nextFloat()));
         final List<DataPointWithMetricID> datapoints = new ArrayList<DataPointWithMetricID>();
@@ -77,7 +99,9 @@ public class TimeSeriesWorkload extends Workload {
             startTime = endTime;
             endTime = tmp;
         }
-        final String field = fieldPrefix + rand.nextInt(fieldCount);
+        final int id = rand.nextInt(fieldCount * measurementCount);
+        final String measurement = getMeasurementName(id);
+        final String field = getFieldName(id);
         if (db.scanDatapoints(table, measurement, field, startTime, endTime,
                 timeUnit, new Vector<DataPoint>()) == 0) {
             return true;
