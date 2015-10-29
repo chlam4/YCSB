95cd3cbc2ffeea46402270b8f30d73f29dec61d0
Cheuk Lam <cheuk.lam@emc.com>
2015-10-29 12:05:07 -0400

    Added support for multiple fields.

--- core/src/main/java/com/yahoo/ycsb/workloads/TimeSeriesWorkload.java
+++ core/src/main/java/com/yahoo/ycsb/workloads/TimeSeriesWorkload.java
@@ -3,8 +3,10 @@ package com.yahoo.ycsb.workloads;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.util.Random;
 import java.util.Vector;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import com.yahoo.ycsb.DB;
 import com.yahoo.ycsb.FloatByteIterator;
@@ -26,14 +28,18 @@ public class TimeSeriesWorkload extends Workload {
     private TimeUnit timeUnit;
     private String table;
     private String measurement;
-    private String field;
+    private String fieldPrefix;
+    private int fieldCount;
+    private static final AtomicInteger currFieldIndex = new AtomicInteger();
+    private static final Random rand = new Random();
 
     @Override
     public void init(Properties p) throws WorkloadException {
         timeUnit = TimeUnit.valueOf(p.getProperty("tsdb.timeUnit", "MILLISECONDS"));
         table = p.getProperty("tsdb.table", "mydb");
         measurement = p.getProperty("tsdb.measurement", "measurement1");
-        field = p.getProperty("tsdb.field", "field1");
+        fieldPrefix = p.getProperty("tsdb.field.prefix", "field");
+        fieldCount = Integer.parseInt(p.getProperty("tsdb.field.count", "1"));
         final String floatGeneratorName = p.getProperty("tsdb.floatGenerator", "fixed");
         final float lower = Float.parseFloat(p.getProperty("tsdb.floatGenerator.lower", "0.0"));
         final float upper = Float.parseFloat(p.getProperty("tsdb.floatGenerator.upper", "1.0"));
@@ -50,6 +56,7 @@ public class TimeSeriesWorkload extends Workload {
 
     @Override
     public boolean doInsert(DB db, Object threadstate) {
+        final String field = fieldPrefix + currFieldIndex.getAndIncrement()%fieldCount;
         final DataPointWithMetricID dp = new DataPointWithMetricID(
                 field, loadTS.next(), new FloatByteIterator(floatGenerator.nextFloat()));
         final List<DataPointWithMetricID> datapoints = new ArrayList<DataPointWithMetricID>();
@@ -70,6 +77,7 @@ public class TimeSeriesWorkload extends Workload {
             startTime = endTime;
             endTime = tmp;
         }
+        final String field = fieldPrefix + rand.nextInt(fieldCount);
         if (db.scanDatapoints(table, measurement, field, startTime, endTime,
                 timeUnit, new Vector<DataPoint>()) == 0) {
             return true;
